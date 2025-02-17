7dc99a652c4e1868bac9edd481d5ac5bbcd1f1e4
alteaSC <seguracaliz@gmail.com>
2025-02-17 13:18:21 +0100

    last commit

--- src/utils/authAPI.js
+++ src/utils/authAPI.js
@@ -1,3 +1,13 @@
+const getRedirectUri = () => {
+  if (window.location.hostname.includes("vercel.app")) {
+    return "https://proyecto-12-spotify-2-0.vercel.app/callback";
+  } else if (window.location.hostname.includes("netlify.app")) {
+    return "https://spotify-2-0.netlify.app/callback";
+  } else {
+    return "http://localhost:5173/callback"; // Para desarrollo local
+  }
+}
+
 const CLIENT_ID = '72da854640234c3f8f13b79561eee180'
 const REDIRECT_URI = getRedirectUri()
 const SCOPES = [
@@ -8,16 +18,6 @@ const SCOPES = [
   'playlist-read-private'
   // Agrega más scopes según lo que necesites
 ]
-
-const getRedirectUri = () => {
-  if (window.location.hostname.includes("vercel.app")) {
-    return "https://proyecto-12-spotify-2-0.vercel.app/callback";
-  } else if (window.location.hostname.includes("netlify.app")) {
-    return "https://spotify-2-0.netlify.app/callback";
-  } else {
-    return "http://localhost:5173/callback"; // Para desarrollo local
-  }
-}
 
 export const getSpotifyAuthUrl = () => {
   const authUrl = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${encodeURIComponent(
