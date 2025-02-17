4ebf7bd65047145b67c34f5840daeb6478fca6a4
alteaSC <seguracaliz@gmail.com>
2025-02-17 13:15:02 +0100

    authAPI commit

--- src/utils/authAPI.js
+++ src/utils/authAPI.js
@@ -1,5 +1,5 @@
 const CLIENT_ID = '72da854640234c3f8f13b79561eee180'
-const REDIRECT_URI = 'http://[::1]:8000/callback'
+const REDIRECT_URI = getRedirectUri()
 const SCOPES = [
   'user-read-private',
   'user-read-email',
@@ -8,6 +8,16 @@ const SCOPES = [
   'playlist-read-private'
   // Agrega más scopes según lo que necesites
 ]
+
+const getRedirectUri = () => {
+  if (window.location.hostname.includes("vercel.app")) {
+    return "https://proyecto-12-spotify-2-0.vercel.app/callback";
+  } else if (window.location.hostname.includes("netlify.app")) {
+    return "https://spotify-2-0.netlify.app/callback";
+  } else {
+    return "http://localhost:5173/callback"; // Para desarrollo local
+  }
+}
 
 export const getSpotifyAuthUrl = () => {
   const authUrl = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${encodeURIComponent(
